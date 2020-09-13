64cc5b98717c672ba8d3538424fefa529b9128bc
SaqibAzim <saqib.azim@viftech.com.pk>
2020-09-13 05:41:57 +0500

    dataview
    asdasd

---
+++ server.js
@@ -0,0 +1,107 @@
+﻿require('rootpath')();
+var expressfileupload  = require('express-fileupload');
+const express = require('express');
+const ejs = require('ejs');
+var path = require('path');
+const paypal = require('paypal-rest-sdk');
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'AYjBaKtupv5peM-INo9F71PUrEUmcL0clkA0IDBOspPxa1TaLm5aH5bE0gVDj0uU-6URjQlrfNfUuZAK',
+    'client_secret': 'EDSN1jLSY3yntoSL4pFvZ9YZyMX8LtCpdD70qIDNm5Zi91BtYVofd5x5mOSn17OakVq5F1yi9vVKYfN7'
+  });
+
+const app = express();
+app.use(expressfileupload());
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorHandler = require('_middleware/error-handler');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+// api routes
+app.use('/users', require('./users/users.controller'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/content')));
+app.get('/', (req, res) => res.render('index'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/signup', (req, res) => res.render('signup'));
+app.get('/forget', (req, res) => res.render('forget'));
+app.get('/resetpassword', (req, res) => res.render('resetPassword'));
+
+app.post('/pay', (req, res) => {
+    const create_payment_json = {
+      "intent": "sale",
+      "payer": {
+          "payment_method": "paypal"
+      },
+      "redirect_urls": {
+          "return_url": "http://localhost:3000/success",
+          "cancel_url": "http://localhost:3000/cancel"
+      },
+      "transactions": [{
+          "item_list": {
+              "items": [{
+                  "name": "Red Sox Hat",
+                  "sku": "001",
+                  "price": "25.00",
+                  "currency": "USD",
+                  "quantity": 1
+              }]
+          },
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          },
+          "description": "Hat for the best team ever"
+      }]
+  };
+  
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+        throw error;
+    } else {
+        for(let i = 0;i < payment.links.length;i++){
+          if(payment.links[i].rel === 'approval_url'){
+            res.redirect(payment.links[i].href);
+          }
+        }
+    }
+  });
+  
+  });
+  
+  app.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+          "amount": {
+              "currency": "USD",
+              "total": "25.00"
+          }
+      }]
+    };
+  
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      if (error) {
+          console.log(error.response);
+          throw error;
+      } else {
+          console.log(JSON.stringify(payment));
+          res.send('Success');
+      }
+  });
+  });
+  
+  app.get('/cancel', (req, res) => res.send('Cancelled'));
+
+
+// global error handler
+app.use(errorHandler);
+
+// start server
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 3000;
+app.listen(port, () => console.log('Server listening on port ' + port));
---
+++ users/user.service.js
@@ -0,0 +1,185 @@
+﻿const config = require('config.json');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const ddb = require('../server.js');
+const nodemailer = require("nodemailer");
+const { param } = require('./users.controller.js');
+var CryptoJS = require("crypto-js");
+const { Sequelize } = require('sequelize');
+var jwt_decode = require('jwt-decode');
+const { v4: uuidv4 } = require('uuid');
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete,
+    forget,
+    resetPassword
+};
+
+async function authenticate({ username, password }) {
+    const user = await db.User.scope('withHash').findOne({ where: { username } });
+
+    if (!user || !(await bcrypt.compare(password, user.hash)))
+        throw 'Username or password is incorrect';
+
+    // authentication successful
+    const token = jwt.sign({ sub: user.id }, config.secret, { expiresIn: '7d' });
+    return { ...omitHash(user.get()), token };
+    router.get('/', authorize(), getAll);
+
+
+}
+
+async function getAll() {
+    return await db.User.findAll();
+}
+
+async function getById(id) {
+    return await getUser(id);
+}
+
+async function create(params) {
+    // validate
+console.log(params.body+ "body",params.files)
+     if (await db.User.findOne({ where: { username: params.body.username } })) {
+         throw 'Username "' + params.body.username + '" is already taken';
+     }
+
+    // hash password
+    if (params.body.password) {
+        params.body.hash = await bcrypt.hash(params.body.password, 10);
+    }
+//img upload 
+    const file = params.files.imgPath;
+    const  name = uuidv4()+'.'+file.name.split('.')[1];
+    file.mv("./uploads/"+ name, function (err, result) {
+        if (err)
+            throw err;
+    })
+    // save user
+
+    var dataPost = {
+        firstName: params.body.firstName,
+        lastName: params.body.lastName,
+        username: params.body.username,
+        hash: params.body.hash,
+        phoneNo: params.body.phoneNo,
+        imgPath: name
+    };
+
+    await db.User.create(dataPost);
+
+}
+
+async function update(id, params) {
+    const user = await getUser(id);
+
+    // validate
+    const usernameChanged = params.username && user.username !== params.username;
+    if (usernameChanged && await db.User.findOne({ where: { username: params.username } })) {
+        throw 'Username "' + params.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (params.password) {
+        params.hash = await bcrypt.hash(params.password, 10);
+    }
+
+    // copy params to user and save
+    Object.assign(user, params);
+    await user.save();
+
+    return omitHash(user.get());
+}
+
+async function forget(params) {
+    console.log(params,"params");
+    
+   var data = await db.User.findOne({ where: { username: params.email }});
+console.log(data,"data")
+    if (data != "" &&  data != null ) {
+        emailSend(params);
+        //throw 'link sent';
+    }
+    else{
+        throw 'Email does not match with any eamil.';
+    }
+
+   // return omitHash(user.get());
+}
+
+async function  resetPassword(params) {
+    //var email = jwt_decode(params.body.id,{payload:true});
+    var email = params.body.id;
+    //console.log(email.payload.email.email,'1');
+    const sequelize = new Sequelize('gameshow', 'root', null, {
+        dialect: 'mysql'
+      })
+     // console.log(email.payload.email,'2');
+       const pass = await bcrypt.hash(params.body.password, 10);
+      const records = await sequelize.query(`UPDATE users SET hash='${pass}' WHERE username='${email}'`, {
+          //type: QueryTypes.UPDATE
+
+        }); 
+       console.log(email,'3');
+//console.log(JSON.stringify(records[0], null, 2));
+
+}
+
+async function _delete(id) {
+    const user = await getUser(id);
+    await user.destroy();
+}
+
+// helper functions
+
+async function getUser(id) {
+    const user = await db.User.findByPk(id);
+    if (!user) throw 'User not found';
+    return user;
+}
+
+function omitHash(user) {
+    const { hash, ...userWithoutHash } = user;
+   
+console.log(user,".......");
+    return userWithoutHash;
+}
+
+
+async function emailSend(d) {
+    
+    const payload = {email:d}
+    
+    const token = jwt.sign({payload},'mykey');
+    console.log(d,'1')
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: '',
+          pass: '18anatomytj2'
+        }
+      });
+      console.log(d,'2')
+      var mailOptions = {
+        from: 'your email',
+        to: d.email,
+        subject: 'Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+        'http://localhost:3000/resetpassword \n\n' +
+        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      console.log(d,'3')
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error,"error");
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      }); 
+  }
